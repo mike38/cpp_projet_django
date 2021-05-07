b484c77272b0b73d242b917bc76a616637cd7b9c
Milena <milena.sof@aol.fr>
2021-05-07 11:41:07 +0200

    Ajout de l'app taggit

--- projet_cpp/projet_cpp/settings.py
+++ projet_cpp/projet_cpp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'exo',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+TAGGIT_CASE_INSENSITIVE = True
